110d8d31595c575237d80362ef2372238b6f84c7
Lindo-code <sdrowvieli1@gmail.com>
2026-03-26 10:36:21 +0200

    company name logo update

--- components/landing/CompanyName.tsx
+++ components/landing/CompanyName.tsx
@@ -31,35 +31,88 @@ export default function CompanyName() {
               className="company-name-logo-pop mx-[0.04em] -mt-1 inline-flex items-end"
               style={{ animationDelay: `${index * 45}ms` }}
             >
-              {/* <span className="hidden min-[641px]:inline-flex"> */}
-              {/*   <Logo */}
-              {/*     size="large" */}
-              {/*     scale={1.45} */}
-              {/*     postMargin={30} */}
-              {/*     headPos={0.5} */}
-              {/*     rayPos={1.5} */}
-              {/*   /> */}
-              {/* </span> */}
+              <span
+                key="logo-i"
+                className="company-name-logo-pop mx-[0.04em] -mt-1 inline-flex items-end"
+                style={{ animationDelay: `${index * 45}ms` }}
+              >
+                {/* 0 - 639 */}
+                <span className="inline-flex min-[640px]:hidden">
+                  <Logo
+                    size="medium"
+                    scale={1}
+                    postMargin={40}
+                    headPos={0.3}
+                    rayPos={1.2}
+                  />
+                </span>
 
-              <span className="hidden sm:inline-flex min-[641px]:hidden">
-                <Logo
-                  size="compact-xs"
-                  scale={0.6}
-                  postMargin={35}
-                  headPos={0.3}
-                  rayPos={1.2}
-                />
-              </span>
+                {/* 640 - 767 */}
+                <span className="hidden min-[640px]:inline-flex min-[768px]:hidden">
+                  <Logo
+                    size="compact-xs"
+                    scale={0.6}
+                    postMargin={35}
+                    headPos={0.3}
+                    rayPos={1.2}
+                  />
+                </span>
 
-              <span className="inline-flex sm:hidden">
-                <Logo
-                  size="medium"
-                  scale={1}
-                  postMargin={40}
-                  headPos={0.3}
-                  rayPos={1.2}
-                />
-              </span>
+                {/* 768 - 899 */}
+                <span className="hidden min-[768px]:inline-flex min-[900px]:hidden">
+                  <Logo
+                    size="medium"
+                    scale={1.02}
+                    postMargin={38}
+                    headPos={0.34}
+                    rayPos={1.24}
+                  />
+                </span>
+
+                {/* 900 - 1023 */}
+                <span className="hidden min-[900px]:inline-flex min-[1024px]:hidden">
+                  <Logo
+                    size="medium"
+                    scale={1.12}
+                    postMargin={36}
+                    headPos={0.38}
+                    rayPos={1.28}
+                  />
+                </span>
+
+                {/* 1024 - 1151 */}
+                <span className="hidden min-[1024px]:inline-flex min-[1152px]:hidden">
+                  <Logo
+                    size="large"
+                    scale={1.16}
+                    postMargin={34}
+                    headPos={0.42}
+                    rayPos={1.34}
+                  />
+                </span>
+
+                {/* 1152 - 1279 */}
+                <span className="hidden min-[1152px]:inline-flex min-[1280px]:hidden">
+                  <Logo
+                    size="large"
+                    scale={1.26}
+                    postMargin={32}
+                    headPos={0.46}
+                    rayPos={1.4}
+                  />
+                </span>
+
+                {/* 1280+ */}
+                <span className="hidden min-[1280px]:inline-flex">
+                  <Logo
+                    size="large"
+                    scale={1.45}
+                    postMargin={30}
+                    headPos={0.5}
+                    rayPos={1.5}
+                  />
+                </span>
+              </span>{" "}
             </span>
           ) : (
             <span
